baafa49127d8e8c617d9d441f406cdb0eefb7d5d
He Jiayou <jhe61@illinois.edu>
2026-05-28 22:08:41 -0500

    remote

---
+++ lua/plugins/remote.lua
@@ -0,0 +1,15 @@
+
+return {
+  {
+    "amitds1997/remote-nvim.nvim",
+    version = "*", -- Use latest stable version
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+    opts = {
+      -- Add your configuration here
+    },
+  },
+}
